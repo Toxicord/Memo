1ad8e93dbef1d5ad3266bfcf431df79329c4f111
ny3740ra <ny3740ra@e1710-as1664978.century.edu>
2019-11-06 08:26:44 -0600

    FontDemo

--- Memo/src/src/FontDemo.java
+++ Memo/src/src/FontDemo.java
@@ -1,5 +1,61 @@
 package src;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.JComboBox;
+
 public class FontDemo {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FontDemo window = new FontDemo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FontDemo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(53, 41, 335, 231);
+		frame.getContentPane().add(textPane);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(53, 6, 76, 27);
+		frame.getContentPane().add(comboBox);
+		String[] fontList = {"Serif", "Times New Roman", "Arial"};
+		for (int i = 0; i < fontList.length; i ++) {
+			comboBox.addItem((String) fontList[i]);
+		}
+		
+		
+		
+		
+	}
 }
