93eb71bdc1785dcef51410267bbe6be3f21a5b0a
kingstone1927 <hoangthach1927@gmail.com>
2019-11-14 00:21:49 -0600

    redo

---
+++ Memo/src/Memo_redo.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class Memo_redo implements ActionListener{
+	private JFrame mainFrame = new JFrame();
+	
+	JMenuBar menuBar = new JMenuBar();
+	JMenu mnFile = new JMenu("File");
+	JMenu mnWindows = new JMenu("Windows");
+	JMenuItem mnSave = new JMenuItem("Save");
+	JMenuItem mnOpen = new JMenuItem("Open");
+	
+	JPanel textPanel = new JPanel();
+	
+	JTabbedPane textTabbedPane = new JTabbedPane();
+	
+	JEditorPane[] textEdittor;
+	
+	JButton addTabButton = new JButton("Add new tab");
+	
+	public static void main(String[] args) {
+		Memo_redo DemoMemo = new Memo_redo("Demo");
+	}
+	
+	public Memo_redo(String title) {
+		mainFrame.setTitle(title);
+		mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(400,200);
+		mainFrame.setLayout(new BorderLayout());
+		
+		createMenuBar();
+		createTextMemo();
+		mainFrame.setVisible(true);
+	}
+	private void createTextMemo() {
+		// "Add Tab" button for the tab pane
+		textTabbedPane.setComponentAt(0, addTabButton);
+		addTabButton.addActionListener(this);
+		
+	}
+
+	private void createMenuBar() {
+		mnFile.add(mnSave);
+		mnFile.add(mnOpen);
+		menuBar.add(mnFile);
+		menuBar.add(mnWindows);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String buttonClicked = e.getActionCommand();
+		
+		if (buttonClicked.equalsIgnoreCase("Add new tab")) {
+			
+		}
+		
+	}
+}
