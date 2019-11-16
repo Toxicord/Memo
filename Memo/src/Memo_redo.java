b32e027a53ae941517520eea3f6a58cc3b51b2c0
kingstone1927 <hoangthach1927@gmail.com>
2019-11-16 09:31:38 -0600

    look great

--- Memo/src/Memo_redo.java
+++ Memo/src/Memo_redo.java
@@ -3,36 +3,46 @@
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Memo_redo implements ActionListener{
 	private JFrame mainFrame = new JFrame();
 	
+	// all components for the menu bar are defined from here:
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu mnFile = new JMenu("File");
 	private JMenu mnWindows = new JMenu("Windows");
 	private JMenuItem mnSave = new JMenuItem("Save");
 	private JMenuItem mnOpen = new JMenuItem("Open");
+	// all components for the menu bar end here:
+
 	
 	private JPanel textPanel = new JPanel();
 	private JPanel toolPanel = new JPanel();
+	private JPanel northPanel = new JPanel();
 	
 	private JTabbedPane textTabbedPane = new JTabbedPane();
 	
-	private JTextPane[] textEditor = new JTextPane[100];
+	// An array of Text Pane
+	private JTextPane[] textPane = new JTextPane[100];
 	// Total number of tabs to keep track of the numbers of tabs
 	private int totalNumbTab = 1;
 	
-	private JButton addTabButton = new JButton("Add new tab");
-	private JButton boldButton = new JButton("B");
-	private JButton italicButton = new JButton("I");
-	private JButton underlineButton = new JButton("U");
-	
-	
-	private JToolBar sideToolBar = new JToolBar(JToolBar.VERTICAL);
+	// all components for the tools bar are defined from here:
+	private JToolBar toolBar = new JToolBar();
 	
 	private JComboBox fontComboBox = new JComboBox();
 	private JComboBox highlightColor = new JComboBox();
+	
+	private JButton addTabButton = new JButton("Add new tab");
+	ImageIcon bold = new ImageIcon("src/bold.jpg");
+	private JButton boldButton = new JButton(bold);
+	ImageIcon italic = new ImageIcon("src/italic.jpg");
+	private JButton italicButton = new JButton(italic);
+	ImageIcon under = new ImageIcon("src/under.jpg");
+	private JButton underlineButton = new JButton(under);
+	// all components for the tools bar end here.
 	
 	
 	public static void main(String[] args) {
@@ -45,36 +55,51 @@ public class Memo_redo implements ActionListener{
 		mainFrame.setSize(400,200);
 		mainFrame.setLayout(new BorderLayout());
 		
-		
 		createMenuBar();
 		createTextPanel();
 		createToolBar();
+		createNorthPanel();
 		
 		createFontComboBox();
 		
-		mainFrame.add(menuBar, BorderLayout.NORTH);
+		mainFrame.add(northPanel, BorderLayout.NORTH);
 		mainFrame.add(textPanel, BorderLayout.CENTER);
-		mainFrame.add(toolPanel, BorderLayout.WEST);
-
 		
 		mainFrame.setVisible(true);
 	}
 	
-	private void createFontComboBox() {
-		
+	private void createNorthPanel() {
+		northPanel.setLayout(new GridLayout(2,1));
+		northPanel.add(menuBar);
+		northPanel.add(toolPanel);		
 	}
 
+	private void createFontComboBox() {
+		String[] fontList = {"Arial", "Times New Roman", "Courier New", "Arial Black"};
+		for (int i = 0; i < fontList.length; i++) {
+			fontComboBox.addItem(fontList[i]);
+		}
+	}
+
+	
 	private void createToolBar() {
 		toolPanel.setLayout(new BorderLayout());
-		sideToolBar.setRollover(true);
-		sideToolBar.addSeparator();
-		sideToolBar.add(fontComboBox);
-		sideToolBar.add(boldButton);
-		sideToolBar.add(italicButton);
-		sideToolBar.add(underlineButton);
-		sideToolBar.add(highlightColor);
 		
-		toolPanel.add(sideToolBar);
+		// Set layout and other features for the tool bar
+		toolBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+		toolBar.setRollover(true);
+		toolBar.addSeparator();
+		
+		// Add tools to the tool bar
+		toolBar.add(fontComboBox);
+		boldButton.setLayout(new GridLayout(1,1));
+		toolBar.add(boldButton);
+		toolBar.add(italicButton);
+		toolBar.add(underlineButton);
+		toolBar.add(highlightColor);
+		
+		// Add tool bar to the toolPanel
+		toolPanel.add(toolBar);
 	}
 	
 
@@ -85,9 +110,9 @@ public class Memo_redo implements ActionListener{
 
 		
 		// Create the first TextPane
-		textEditor[0] = new JTextPane();
+		textPane[0] = new JTextPane();
 		// Initialize the first tab with a TextPane
-		textTabbedPane.addTab("New Tab", textEditor[0]);
+		textTabbedPane.addTab("New Tab", textPane[0]);
 		
 		// "Add Tab" button for the tab pane, and add actionListener to the button
 		textTabbedPane.addTab(null, null);
@@ -111,9 +136,9 @@ public class Memo_redo implements ActionListener{
 		
 		if (buttonClicked.equalsIgnoreCase("Add new tab")) {
 			// Create new TextPane
-			textEditor[totalNumbTab] = new JTextPane();
+			textPane[totalNumbTab] = new JTextPane();
 			// Insert the new TextPane above to the latest tab
-			textTabbedPane.insertTab("New tab", null, textEditor[totalNumbTab], null, textTabbedPane.getTabCount() - 1);
+			textTabbedPane.insertTab("New tab", null, textPane[totalNumbTab], null, textTabbedPane.getTabCount() - 1);
 		}
 		
 	}
