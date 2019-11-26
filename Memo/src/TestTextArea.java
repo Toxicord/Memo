00ef20fc682a4752c5c5fe204a47f7190fd1636f
bw2773dt <bw2773dt@W3165-S1904062.century.edu>
2019-11-25 18:14:27 -0600

    save first file now working.

--- Memo/src/TestTextArea.java
+++ Memo/src/TestTextArea.java
@@ -1,145 +1,236 @@
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
-public class TestTextArea {
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
 
-	private JFrame frame;
-	private JTextArea tArea;
-    private JTextPane jpane;
-    private JButton save;
+public class TestTextArea implements ActionListener{
+	private JFrame mainFrame = new JFrame();
+	
+	// all components for the menu bar are defined from here:
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu mnFile = new JMenu("File");
+	private JMenu mnWindows = new JMenu("Windows");
+	private JMenuItem mnSave = new JMenuItem("Save");
+	private JMenuItem mnOpen = new JMenuItem("Open");
+	// all components for the menu bar end here:
 
-    protected void initUI() {
-        JFrame frame = new JFrame(TestTextArea.class.getSimpleName());
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(0, 0, 1000, 1000);
-        frame.getContentPane().setLayout(null);
-        
-        jpane = new JTextPane();
-        
-        JPanel panel = new JPanel();
-		panel.setBounds(168, 72, 605, 704);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 22, 585, 657);
-		panel.add(tabbedPane);
-		
-        Font font = new Font("Serif", Font.ITALIC, 20);
-        JTextArea tArea = new JTextArea(40,5);
-        JScrollPane scrollPane = new JScrollPane(tArea);
-        tabbedPane.addTab("New tab", null, scrollPane, null);
-        
-        JTextPane textPane_2 = new JTextPane();
-        tabbedPane.addTab("New tab", null, textPane_2, null);
-        
-        JToolBar toolBar = new JToolBar();
-        toolBar.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-        toolBar.setBounds(61, 22, 64, 24);
-        frame.getContentPane().add(toolBar);
-        
-        /* Combo box for Font*/
-        JComboBox comboBox = new JComboBox();
-        comboBox.addMouseListener(new MouseAdapter() {
-        	@Override
-        	public void mousePressed(MouseEvent e) {
-        		scrollPane.setFont(font);
-        	}
-        });
-        comboBox.setModel(new DefaultComboBoxModel(new String[] {"Arial", "Tahoma"}));
-        comboBox.setFont(new Font("Tahoma", Font.PLAIN, 11));
-        toolBar.add(comboBox);
-        
-        TextArea text = new TextArea();
-        jpane.setFont(font);
-        jpane.setForeground(Color.blue);
-        jpane.add(text, BorderLayout.CENTER);
-		
-		JButton btnNewButton_1 = new JButton("+");
-		tabbedPane.setTabComponentAt(1, btnNewButton_1);
-		
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				JTextArea textArea = new JTextArea(40,5);
-				JScrollPane scrollPane1 = new JScrollPane(textArea);
-				tabbedPane.insertTab("New tab", null, scrollPane1, null, tabbedPane.getTabCount() - 1);
-			}
+	
+	private JPanel textPanel = new JPanel();
+	private JPanel toolPanel = new JPanel();
+	private JPanel northPanel = new JPanel();
+	
+	private JTabbedPane textTabbedPane = new JTabbedPane();
+	
+	// An array of Text Pane
+	private JTextPane[] textPane = new JTextPane[100];
+	// Total number of tabs to keep track of the numbers of tabs
+	private int totalNumbTab = 1;
+	
+	// all components for the tools bar are defined from here:
+	private JToolBar toolBar = new JToolBar();
+	
+	private JComboBox fontComboBox = new JComboBox();
+	private JComboBox highlightColor = new JComboBox();
+	
+	private JButton addTabButton = new JButton("Add new tab");
+	ImageIcon bold = new ImageIcon("src/bold.jpg");
+	private JButton boldButton = new JButton(bold);
+	ImageIcon italic = new ImageIcon("src/italic.jpg");
+	private JButton italicButton = new JButton(italic);
+	ImageIcon under = new ImageIcon("src/under.jpg");
+	private JButton underlineButton = new JButton(under);
+	// all components for the tools bar end here.
+	
+	
+	
+	private Highlighter.HighlightPainter grayPainter;
+	private Highlighter.HighlightPainter yellowPainter;
+	private Highlighter.HighlightPainter redPainter;
+	private Highlighter.HighlightPainter cyanPainter;
+	private Highlighter.HighlightPainter orangePainter;
+	private Highlighter.HighlightPainter pinkPainter;
+	private int firstUpdateIndex;
+	private int counter;
 
-		});
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		JMenu mnFile = new JMenu("File");
+	private Map<Integer, Highlighter.Highlight> highlights;
+	
+	public static void main(String[] args) {
+		TestTextArea DemoMemo = new TestTextArea("Demo");
+	}
+	
+	public TestTextArea(String title) {
+		mainFrame.setTitle(title);
+		mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(400,200);
+		mainFrame.setLayout(new BorderLayout());
+		
+		createMenuBar();
+		createTextPanel();
+		createToolBar();
+		createNorthPanel();
+		
+		createFontComboBox();
+		
+		mainFrame.add(northPanel, BorderLayout.NORTH);
+		mainFrame.add(textPanel, BorderLayout.CENTER);
+		
+		mainFrame.setVisible(true);
+		
+	}
+
+	private void createNorthPanel() {
+		northPanel.setLayout(new GridLayout(2,1));
+		northPanel.add(menuBar);
+		northPanel.add(toolPanel);		
+	}
+
+	private void createFontComboBox() {
+		String[] fontList = {"Arial", "Times New Roman", "Courier New", "Arial Black"};
+		for (int i = 0; i < fontList.length; i++) {
+			fontComboBox.addItem(fontList[i]);
+		}
+	}
+	private void createHighlightComboBox() {
+    String[] colorOptions = { "GRAY", "YELLOW", "RED", "CYAN", "ORANGE", "PINK" };
+		for (int i = 0; i < colorOptions.length; i++) {
+			highlightColor.addItem(colorOptions[i]);
+		}
+	}	
+	
+	private void TextHighlight() {
+		grayPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.GRAY);
+		yellowPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
+		redPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.RED);
+		cyanPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.CYAN);
+		orangePainter = new DefaultHighlighter.DefaultHighlightPainter(Color.ORANGE);
+		pinkPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.PINK);
+		firstUpdateIndex = -1;
+		counter = 0;
+	}
+	private void createToolBar() {
+		toolPanel.setLayout(new BorderLayout());
+		
+		// Set layout and other features for the tool bar
+		toolBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+		toolBar.setRollover(true);
+		toolBar.addSeparator();
+		
+		// Add tools to the tool bar
+		toolBar.add(fontComboBox);
+		boldButton.setLayout(new GridLayout(1,1));
+		toolBar.add(boldButton);
+		toolBar.add(italicButton);
+		toolBar.add(underlineButton);
+		toolBar.add(highlightColor);
+		
+		// Add tool bar to the toolPanel
+		toolPanel.add(toolBar);
+	}
+	
+
+	private void createTextPanel() {
+		// Set layout and bounds for the TextPanel that contains text editing area
+		textPanel.setLayout(new BorderLayout());
+		textPanel.setBounds(168, 72, 605, 704);
+
+		
+		// Create the first TextPane
+		textPane[0] = new JTextPane();
+		// Initialize the first tab with a TextPane
+		textTabbedPane.addTab("New Tab", textPane[0]);
+		
+		// "Add Tab" button for the tab pane, and add actionListener to the button
+		textTabbedPane.addTab(null, null);
+		textTabbedPane.setTabComponentAt(1, addTabButton);
+		addTabButton.addActionListener(this);
+		
+		textPanel.add(textTabbedPane);
+	}
+
+	private void createMenuBar() {
+		mnFile.add(mnSave);
+		mnFile.add(mnOpen);
 		menuBar.add(mnFile);
-		JMenu mnWindows = new JMenu("Windows");
 		menuBar.add(mnWindows);
-        frame.getContentPane().add(new JScrollPane(jpane));
-        frame.getContentPane().add(toolBar, BorderLayout.SOUTH);
-        save = new JButton("Save file");
-        save.setBounds(135, 23, 87, 23);
-        frame.getContentPane().add(save);
-        
-        save.addActionListener(new ActionListener() {
+		//this adds action to the save and open menuItems
+		mnSave.addActionListener(this); 
+		mnOpen.addActionListener(this);
+	}
+	
+	//this is save
+	private void save(int tabCount) {
+		if(tabCount==0) {
+			//promt user if wants to save
+			String textsPane;
+			if(textPane[tabCount].getText()==null) {
+				textsPane="";
+			}else {
+				textsPane=textPane[tabCount].getText();
+			}
+			JFileChooser saveSel = new JFileChooser();
+			int saveConf = saveSel.showSaveDialog(mnSave);
+	        if (saveConf == JFileChooser.APPROVE_OPTION) {
+	        	 File selFile = saveSel.getSelectedFile();
+	        	 if (!selFile.getName().toLowerCase().endsWith(".txt")) {
+	                	selFile = new File(selFile.getParentFile(), selFile.getName() + ".txt");
+	                }
+	        	 try {
+					FileWriter fWriter = new FileWriter (selFile);
+					PrintWriter pWriter = new PrintWriter (fWriter);
+					pWriter.println (textsPane);
+					pWriter.close();
+	        	 }catch(FileNotFoundException e) {
+	        		 e.printStackTrace();
+	        	 } catch (IOException e) {
+	        		 e.printStackTrace();
+	        	 }
+	        	
+		    }
+		}else if(tabCount>1) {
+			
+		}
+	}
+	//save end
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                saveYourFile();
-            }
-        });
-        frame.setSize(500, 400);
-        frame.setVisible(true);
-    }
-    
-    //PLEASE NOTE THE CODE BELOW IS NOT FUNCTIONING PROPERLY BECAUSE IT IS REFERENCE CODE THAT IS NOT MY OWN (I have all of the resources I've used as placeholders and examples from in a folder for end-of-file accreditation in code comprehension via forum examples)
-    //I'm using this code for comprehension purposes, the final saveYourFile function will not nearly resemble this one. Currently trying to catch multiple exceptions on one line & I absolutely must get this to work on an instance(or pane) basis. 
-    protected void saveYourFile() {
-        JFileChooser fileChooser = new JFileChooser();
-        int retval = fileChooser.showSaveDialog(save);
-        if (retval == JFileChooser.APPROVE_OPTION) {
-            File selFile = fileChooser.getSelectedFile();
-            if (selFile != null) {
-                if (!selFile.getName().toLowerCase().endsWith(".txt")) {
-                	selFile = new File(selFile.getParentFile(), selFile.getName() + ".txt");
-                }
-                try {
-                	jpane.write(new OutputStreamWriter(new FileOutputStream(selFile), "utf-8"));
-                    Desktop.getDesktop().open(selFile);
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                new TestTextArea().initUI();
-            }
-        });
-    }
-    
-}
+	//this is open
+	private void openF() {
+		// TODO Auto-generated method stub
+		
+	}
+	//open end
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String buttonClicked = e.getActionCommand();
+		
+		if (buttonClicked.equalsIgnoreCase("Add new tab")) {
+			// Create new TextPane
+			textPane[totalNumbTab] = new JTextPane();
+			// Insert the new TextPane above to the latest tab
+			textTabbedPane.insertTab("New tab", null, textPane[totalNumbTab], null, textTabbedPane.getTabCount() - 1);
+			totalNumbTab++;//used to increment the tab number. Otherwise it'll only stay in textPane[1]
+		}else if(e.getSource()==mnOpen) {
+			openF();
+		}else if(e.getSource()==mnSave) {
+			save(totalNumbTab-1);
+			}
+		}
+	}
+	
+	
